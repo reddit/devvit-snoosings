2505a97be2a46cf309342b87b1ccb63a78e6b6e1
Stephen Niedzielski <stephen.niedzielski@reddit.com>
2024-10-25 17:20:08 -0600

    drop text from loading screen

--- src/app/preview.tsx
+++ src/app/preview.tsx
@@ -1,21 +1,18 @@
 import {Devvit} from '@devvit/public-api'
 
 export function Preview(): JSX.Element {
-  // to-do: this should be a TV showing static.
+  // to-do: this should be a TV showing static using the five sing! keyboard
+  // colors. ideally, it'd be animated and zap in like a CRT.
   return (
     <vstack width={'100%'} height={'100%'} alignment='center middle'>
       <image
         url='loading.gif'
-        description='Loading…'
+        description='loading…'
         height='140px'
         width='140px'
         imageHeight='240px'
         imageWidth='240px'
       />
-      <spacer size='small' />
-      <text size='large' weight='bold'>
-        We're putting the band back together…
-      </text>
     </vstack>
   )
 }
