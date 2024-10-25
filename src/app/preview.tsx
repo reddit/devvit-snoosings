148bbf54d28ca8c06772231d9b860a26b17555d2
Stephen Niedzielski <stephen.niedzielski@reddit.com>
2024-10-25 17:11:53 -0600

    revert "attempt to implement load screen"
    revert f8e18255f52ec80d1d2e752b899eb3c25eaadafa.

--- src/app/preview.tsx
+++ src/app/preview.tsx
@@ -1,15 +1,21 @@
 import {Devvit} from '@devvit/public-api'
 
 export function Preview(): JSX.Element {
+  // to-do: this should be a TV showing static.
   return (
-    <image
-      url='tv.webp'
-      description='loading…'
-      width='100%'
-      height='100%'
-      imageWidth='756px'
-      imageHeight='512px'
-      resizeMode='fill'
-    />
+    <vstack width={'100%'} height={'100%'} alignment='center middle'>
+      <image
+        url='loading.gif'
+        description='Loading…'
+        height='140px'
+        width='140px'
+        imageHeight='240px'
+        imageWidth='240px'
+      />
+      <spacer size='small' />
+      <text size='large' weight='bold'>
+        We're putting the band back together…
+      </text>
+    </vstack>
   )
 }
